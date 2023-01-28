7571e0a7c5bb29a77c176b9dd6c5c551e8a4be05
ilya.aksenov <ilya.aksenov@demlabs.net>
2023-01-28 17:34:20 +0700

    added flask app

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM python:3.5
 
-RUN apt-get update
-RUN pip install Flask
-
-ADD . /opt/webapp/
+RUN apt-get update && pip install Flask
 
 WORKDIR /opt/webapp
+
+COPY backend.py /opt/webapp/
 
 ENV FLASK_APP=backend.py
 
--- backend.py
+++ backend.py
@@ -1,1 +1,12 @@
-print ('version 0.1')
+from flask import Flask
+import datetime
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "version 0.1-2 " + str(datetime.datetime.now())
+
+if __name__ == '__main__':
+    app.run(debug=True)
